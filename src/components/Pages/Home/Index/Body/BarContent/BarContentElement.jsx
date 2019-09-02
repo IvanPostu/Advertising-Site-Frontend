b2556be8c5c269bf5f1d1ad67199d86bb50f90fd
Ivan <ipostu20000127@gmail.com>
2019-09-02 21:13:07 +0300

    #

--- src/components/Pages/Home/Index/Body/BarContent/BarContentElement.jsx
+++ src/components/Pages/Home/Index/Body/BarContent/BarContentElement.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import style from './BarContent.module.scss'
 
+function priceResult(price, valuta) {
+    if (price === undefined || valuta === undefined) return `Цена: -`
+    return `Цена: ${price} ${valuta}`
+}
 
-const BarContentElement = ({ img, txt }) => (
+const BarContentElement = ({ img, txt, price, valuta }) => (
     <div className={style.contentBox}>
         <div className={style.imgBox}
             style={{ background: `url(${img})`, backgroundSize: '100% 100%' }}></div>
 
         <div className={style.txtBox}>{txt}</div>
 
-        <div className={style.priceBox}></div>
-        <div className={style.btnBox}></div>
+        <div className={style.priceBox}>{priceResult(price, valuta)}</div>
+        <div className={style.boxForBtn}>
+            <button className={style.btn}>
+                {`Подробнее`}
+            </button>
+        </div>
     </div>
 )
 
--- src/components/layouts/BaseLayout/Footer/Footer.stateless.jsx
+++ src/components/layouts/BaseLayout/Footer/Footer.stateless.jsx
@@ -6,11 +6,27 @@ import style from './Footer.module.scss'
 export default function FooterStateless() {
     return (
         <div className={style.footerBody}>
-
             <div className={`${container.container}`}>
-                <h3>Footer</h3>
-            </div>
+                <div id={style.footerBodyID}>
 
+                    <div className={style.footerBox}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nisi sint mollitia sunt quas, quia ducimus impedit praesentium quo corporis quod omnis esse iusto sequi labore distinctio facere porro reiciendis.
+                    </div>
+
+                    <div className={style.footerBox}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nisi sint mollitia sunt quas, quia ducimus impedit praesentium quo corporis quod omnis esse iusto sequi labore distinctio facere porro reiciendis.
+                    </div>
+
+                    <div className={style.footerBox}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nisi sint mollitia sunt quas, quia ducimus impedit praesentium quo corporis quod omnis esse iusto sequi labore distinctio facere porro reiciendis.
+                    </div>
+
+                    <div className={style.footerBox}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nisi sint mollitia sunt quas, quia ducimus impedit praesentium quo corporis quod omnis esse iusto sequi labore distinctio facere porro reiciendis.
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
